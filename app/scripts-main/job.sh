503d83afafc5d1f8a42488bf988ba29bbd30668c
RachelWillis1 <95307943+RachelWillis1@users.noreply.github.com>
2023-09-29 15:44:13 -0600

    Update job.sh

--- app/scripts-main/job.sh
+++ app/scripts-main/job.sh
@@ -8,19 +8,19 @@ echo ----
 echo 1. STALTA triggering
 python3 stalta_trigger.py
 echo ----
-#echo 2.merging events
-#python3 merge_events.py
-#echo ----
-#echo 3.plotting events
+echo 2.merging events
+python3 merge_events.py
+echo ----
+echo 3.plotting events
 # python3 plot_events_from_catalogue.py
-#echo plotting turned off
-#echo ----
-#echo 4.extracting velo features
-#python3 extract_features_velo.py
-#echo ----
-#echo 5.extracting covmat features
-#python3 extract_features_covmat.py
-#echo ---
+echo plotting turned off
+echo ----
+echo 4.extracting velo features
+python3 extract_features_velo.py
+echo ----
+echo 5.extracting covmat features
+python3 extract_features_covmat.py
+echo ---
 
 endall=$(date +%s)
 echo "Elapsed Time: $(($endall-$startall)) seconds"
